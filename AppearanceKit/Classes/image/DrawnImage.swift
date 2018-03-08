3e0f5976f301f9f14bfca1cd4ef020164900c8b6
Georges Boumis <developer.george.boumis@gmail.com>
2018-03-08 15:38:41 +0200

    2.7 Introduce DrawnImage

---
+++ AppearanceKit/Classes/image/DrawnImage.swift
@@ -0,0 +1,78 @@
+//
+//  DrawnImage.swift
+//  AppearanceKit
+//
+//  Created by Georges Boumis on 08/03/2018.
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//
+
+import Foundation
+import UIKit
+import ContentKit
+import QuartzCore
+
+final public class DrawnImage: ContentKit.Image {
+    
+    public typealias DrawBlock = (_ `self`: ContentKit.Image, _ bounds: CGRect, _ context: CGContext) -> Void
+    
+    public struct Options {
+        public let size: ContentKit.Size
+        public let drawBlock: DrawBlock
+    }
+    
+    final private let decorated: ContentKit.Image
+    final private let options: Options
+    
+    public init(decorated: ContentKit.Image, options: Options) {
+        self.decorated = decorated
+        self.options = options
+    }
+    
+    final private lazy var drawnImage: ContentKit.Image = {
+        return autoreleasepool {
+            
+            let size  = self.options.size.asCGSize
+            let rect  = CGRect(origin: CGPoint.zero, size: size)
+            let draw  = self.options.drawBlock
+            let scale = self.decorated.image.scale
+            let image = self.decorated
+            if #available(iOS 10.0,  *) {
+                let renderer = UIGraphicsImageRenderer(size: size)
+                let image = renderer.image(actions: { (context: UIGraphicsImageRendererContext) in
+                    draw(image, rect, context.cgContext)
+                })
+                return AnyImage(image: image)
+            }
+            else {
+                UIGraphicsBeginImageContextWithOptions(size, false, scale)
+                defer { UIGraphicsEndImageContext() }
+                guard let context = UIGraphicsGetCurrentContext() else { return self.decorated }
+                draw(image, rect, context)
+                guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return self.decorated }
+                return AnyImage(image: result)
+            }
+        }
+    }()
+    
+    public var image: UIImage {
+        return self.drawnImage.image
+    }
+}
+
--- AppearanceKit/Classes/image/TintedImage.swift
+++ AppearanceKit/Classes/image/TintedImage.swift
@@ -4,6 +4,24 @@
 //
 //  Created by Georges Boumis on 29/11/2017.
 //
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//
 
 import Foundation
 import UIKit
@@ -62,10 +80,19 @@ public final class TintedImage: ContentKit.Image {
         self.blendMode = blendMode
     }
     
-    public var image: UIImage {
-        return self.decorated
-            .image.tintedImage_blendedImage(usingColor: self.color.color,
-                                            blendMode: self.blendMode.cgBlendMode)!
+    final private lazy var tintedImage: ContentKit.Image = {
+        let color = self.color.color
+        let blendMode = self.blendMode.cgBlendMode
+        let options = DrawnImage.Options(size: self.size) { (image: ContentKit.Image, bounds: CGRect, context: CGContext) in
+            image.image.draw(in: bounds)
+            color.set()
+            UIRectFillUsingBlendMode(bounds, blendMode)
+        }
+        return DrawnImage(decorated: self.decorated, options: options)
+    }()
+    
+    final public var image: UIImage {
+        return self.tintedImage.image
     }
 }
 
@@ -142,33 +169,33 @@ fileprivate extension TintedImage.BlendMode {
     }
 }
 
-fileprivate extension UIImage {
-    
-    fileprivate func tintedImage_blendedImage(usingColor color: UIColor, blendMode: CGBlendMode) -> UIImage? {
-        return autoreleasepool {
-            let size = self.size
-            let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-            let draw = {
-                self.draw(in: rect)
-                color.set()
-            }
-            if #available(iOS 10.0,  *) {
-                let renderer = UIGraphicsImageRenderer(size: size)
-                return renderer.image(actions: { (context: UIGraphicsImageRendererContext) in
-                    draw()
-                    context.fill(rect, blendMode: blendMode)
-                })
-            }
-            else {
-                UIGraphicsBeginImageContextWithOptions(size, false, self.scale)
-                defer { UIGraphicsEndImageContext() }
-                guard let _ = UIGraphicsGetCurrentContext() else { return nil }
-                draw()
-                UIRectFillUsingBlendMode(rect, blendMode)
-                guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
-                return result
-            }
-        }
-    }
-}
+//fileprivate extension UIImage {
+//    
+//    fileprivate func tintedImage_blendedImage(usingColor color: UIColor, blendMode: CGBlendMode) -> UIImage? {
+//        return autoreleasepool {
+//            let size = self.size
+//            let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+//            let draw = {
+//                self.draw(in: rect)
+//                color.set()
+//            }
+//            if #available(iOS 10.0,  *) {
+//                let renderer = UIGraphicsImageRenderer(size: size)
+//                return renderer.image(actions: { (context: UIGraphicsImageRendererContext) in
+//                    draw()
+//                    context.fill(rect, blendMode: blendMode)
+//                })
+//            }
+//            else {
+//                UIGraphicsBeginImageContextWithOptions(size, false, self.scale)
+//                defer { UIGraphicsEndImageContext() }
+//                guard let _ = UIGraphicsGetCurrentContext() else { return nil }
+//                draw()
+//                UIRectFillUsingBlendMode(rect, blendMode)
+//                guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
+//                return result
+//            }
+//        }
+//    }
+//}
 
