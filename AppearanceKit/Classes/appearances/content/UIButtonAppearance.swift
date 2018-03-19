8950ab9b7ae51b0b79902d3e55a1b5c3d1c71b83
Georges Boumis <developer.george.boumis@gmail.com>
2018-03-19 17:26:59 +0200

    2.8.5 fix bug in UIButtonAppearance related to titleLabelAppearance

--- AppearanceKit/Classes/appearances/content/UIButtonAppearance.swift
+++ AppearanceKit/Classes/appearances/content/UIButtonAppearance.swift
@@ -115,6 +115,7 @@ public struct DefaultUIButtonAppearance: UIButtonAppearance {
         self.tintColor = tintColor
         self.shadowColor = shadowColor
         self.shadowOffset = shadowOffset
+        self.titleLabelAppearance = titleLabelAppearance
     }
 }
 
